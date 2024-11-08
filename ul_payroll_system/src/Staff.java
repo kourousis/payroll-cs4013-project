35ea341cfca9bb837724b4b58f81f7eff4d8d56c
ilias <i.k.kourousis@gmail.com>
2024-11-08 13:38:57 +0000

    added javadocs for Staff class and formatted code

--- ul_payroll_system/src/Staff.java
+++ ul_payroll_system/src/Staff.java
@@ -4,6 +4,14 @@ public class Staff {
     protected String email;
     protected Address address;
 
+    /**
+     * Param constructor to create a Staff object
+     * 
+     * @param name
+     * @param phone
+     * @param email
+     * @param address The address of the staff member represented as an object
+     */
     public Staff(String name, String phone, String email, Address address) {
         this.name = name;
         this.phone = phone;
@@ -11,7 +19,12 @@ public class Staff {
         this.address = address;
     }
 
-    // toString
+    /**
+     * Accessor method to get the Staff object as a string in a readable format
+     * 
+     * @return Concatenates the name, phone, email, postcode and address into a
+     *         readable address
+     */
     public String toString() {
         return "Name: " + name + "\nPhone: " + phone + "\nEmail: " + email + "\nAddress: " + address;
     }
