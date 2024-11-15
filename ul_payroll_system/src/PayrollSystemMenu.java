a4a05804af6dc6f4fcde10deb90c2f49c390a4e7
ilias <i.k.kourousis@gmail.com>
2024-11-15 12:30:00 +0000

    init program running class

---
+++ ul_payroll_system/src/PayrollSystemMenu.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PayrollSystemMenu {
+    private Scanner in;
+
+    public PayrollSystemMenu() {
+        in = new Scanner(System.in);
+    }
+
+    public void run() {
+        boolean running = true;
+
+        while (running) {
+
+        }
+    }
+}
