97a91dc991063429d97c24ce259381a807b5b92e
ilias <i.k.kourousis@gmail.com>
2024-11-15 12:30:00 +0000

    init program running class

---
+++ ul_payroll_system/src/PayrollSystem.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class PayrollSystem {
+	public static void main(String[] args) throws IOException {
+		PayrollSystemMenu payrollSystemMenu = new PayrollSystemMenu();
+		payrollSystemMenu.run();
+	}
+}
