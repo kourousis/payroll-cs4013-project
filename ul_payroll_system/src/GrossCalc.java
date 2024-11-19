3b0f877014bacd2fe4295cd03637b6d861b6b813
DanyWatson26 <danykirwan@gmail.com>
2024-11-19 14:43:46 +0000

    gross calc

---
+++ ul_payroll_system/src/GrossCalc.java
@@ -0,0 +1,34 @@
+import java.util.Map;
+
+public class GrossCalc {
+
+    private int years;
+
+
+    public GrossCalc(int yearinput)
+    {
+        this.years = yearinput;
+    }
+
+    //calculate gross based on years worked and job title
+    public void calculateGrossIncome(String department,String jobTitle)
+    {
+        SalaryScale salaryScale2 = new SalaryScale();
+
+        // Gets salary map for department and job title
+        Map<Integer, Float> salaryMap = (Map<Integer,Float>) salaryScale2.getSalaryData(department,jobTitle);
+
+        // Checks salary map for null
+        if (salaryMap != null && salaryMap.containsKey(years)) {
+            float salary = salaryMap.get(years);
+
+            // finds gross income
+            System.out.println("Gross income for " + jobTitle + " in " + department + " department with "
+                    + years + " years of experience is: " + salary);
+
+        } else {
+            // salary data for specifed job or years worked dont exist
+            System.out.println("Salary data not available");
+        }
+    }
+}
