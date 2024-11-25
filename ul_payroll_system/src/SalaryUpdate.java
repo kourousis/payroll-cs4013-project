d0abd715d878e0c1cef9ee35491377ca734471af
LeoB814 <leobrady.2002@gmail.com>
2024-11-25 18:35:37 +0000

    Update SalaryUpdate.java

--- ul_payroll_system/src/SalaryUpdate.java
+++ ul_payroll_system/src/SalaryUpdate.java
@@ -8,31 +8,34 @@ public class SalaryUpdate {
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");  //Changes String from CSV to LocalDate for use
         DBController db = new DBController();
-        // int i = 1;
-        // while(db.GET("employees", i, "EmployeeID") != null){       **Loop for checking every employee**
-        //     i++;
-        // }
-        
 
-        LocalDate hireDate = LocalDate.parse(db.GET("employees", 2, "HireDate"), formatter);
+        LocalDate roleDate = LocalDate.parse(db.GET("employees", 2, "HireDate"), formatter);
 
         LocalDate payBump = LocalDate.parse(db.GET("control_data", 1, "Data"), formatter);
         
         LocalDate today = LocalDate.now();
         System.out.println("Today is: " + today);
         
-        System.out.println("hireDate/rollStartDate: " + hireDate);
+        System.out.println("hireDate/rollStartDate: " + roleDate);
         System.out.println("Next Pay increase date: " + payBump);
         String dateString = payBump.format(formatter);
         
         if(payBump.isBefore(today) || payBump.isEqual(today)){
+
+            int i = 1;
+            int staff = Integer.parseInt(db.GET("employees", i, "EmployeeID"));
+            
+            while(db.GET("employees", i, "EmployeeID") != null){       //**Loop for checking every employee**
+                i++;
+            }
+
             payBump = payBump.plusYears(1);
             dateString = payBump.format(formatter);
             db.UPDATE("control_data", 1, "Data", dateString);
             System.out.println("payBump date updated to " + payBump);
         }
         else{
-            System.out.println("payBump not updated");
+            System.out.println("payBump date not updated");
         }
 
     }
