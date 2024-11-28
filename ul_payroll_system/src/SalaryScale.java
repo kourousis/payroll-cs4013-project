27c9170c240c913013b9f0b22dbc37eeaa3045df
ilias <i.k.kourousis@gmail.com>
2024-11-28 14:12:21 +0000

    formatting

--- ul_payroll_system/src/SalaryScale.java
+++ ul_payroll_system/src/SalaryScale.java
@@ -1,9 +1,7 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public abstract class SalaryScale {
     protected float salary;
     protected String jobTitle;
     protected int years;
+
     public abstract float getSalaryData(String department, String jobTitle, int years);
 }
